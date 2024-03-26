d7d771d9b0144da0f756692e1f15cd6dff4e4a07
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-03-26 15:04:48 +0000

    Add multiple rounds to game
    Keep score externally to minimize function calls. Each round's score on
    separate line, and final result bolded for clarity.

--- rps-script.js
+++ rps-script.js
@@ -3,6 +3,8 @@ const output = document.querySelector("#result-output");
 
 choiceButtons.forEach(currentValue => currentValue.addEventListener("click", playRound));
 
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
     let random = Math.floor(Math.random()*3);
@@ -18,43 +20,68 @@ function getComputerChoice () {
 
 function playRound (event) {
     let playerChoice = event.target.textContent;
-    console.log(playerChoice);
     let computerChoice = getComputerChoice();
-    console.log(computerChoice);
+    
     const result = document.createElement("p");
+    const score = document.createElement("p");
     
     if (playerChoice === computerChoice) {
         result.textContent = `It's a draw! You both played ${playerChoice}.`;
         output.appendChild(result);
+        score.textContent = `Computer : ${computerScore} - ${playerScore} : Player`;
+        output.appendChild(score);
         return;
     }
     switch (playerChoice) {
         case "Rock":
             if (computerChoice === "Paper") {
-                result.textContent = "You lose! Paper covers Rock.";
+                result.textContent = "You lose this round! Paper covers Rock.";
+                computerScore += 1;
                 break;
             } else {
-                result.textContent = "You win! Rock breaks Scissors.";
+                result.textContent = "You win this round! Rock breaks Scissors.";
+                playerScore += 1;
                 break;
             }            
         case "Paper":
             if (computerChoice === "Scissors") {
-                result.textContent = "You lose! Scissors cut Paper.";
+                result.textContent = "You lose this round! Scissors cut Paper.";
+                computerScore += 1;
                 break;
             } else {
-                result.textContent = "You win! Paper covers Rock.";
+                result.textContent = "You win this round! Paper covers Rock.";
+                playerScore += 1;
                 break;       
             }
         default:
             if (computerChoice === "Rock") {
-                result.textContent = "You lose! Rock breaks Scissors.";
+                result.textContent = "You lose this round! Rock breaks Scissors.";
+                computerScore += 1;
                 break;
             } else {
-                result.textContent = "You win! Scissors cut Paper.";
+                result.textContent = "You win this round! Scissors cut Paper.";
+                playerScore += 1;
                 break;        
             }
     }
     output.appendChild(result);
+    score.textContent = `Computer : ${computerScore} - ${playerScore} : Player`;
+    output.appendChild(score);
+    if (computerScore === 5) {
+        const finalResult = document.createElement("p");
+        finalResult.textContent = "Oh no, the computer won 5 times! You lose the game..."
+        finalResult.style.fontWeight = 900;
+        output.appendChild(finalResult);
+        computerScore = 0;
+        playerScore = 0;
+    } else if (playerScore === 5) {
+        const finalResult = document.createElement("p");
+        finalResult.textContent = "Hurray! With five wins, you are the victor :)"
+        output.appendChild(finalResult);
+        finalResult.style.fontWeight = 900;
+        computerScore = 0;
+        playerScore = 0;
+    }
     return;
 }
 
