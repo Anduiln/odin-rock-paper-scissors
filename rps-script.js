0d62e25195e41bd255aa38b92a6ff3fb324d67dc
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-03-26 14:38:18 +0000

    Refactor to use DOM manipulation
    Following TOP course instructions, using DOM methods to play the game
    and output the result. Only a single round for now.

--- rps-script.js
+++ rps-script.js
@@ -1,3 +1,9 @@
+const choiceButtons = document.querySelectorAll(".rps-player-choice");
+const output = document.querySelector("#result-output");
+
+choiceButtons.forEach(currentValue => currentValue.addEventListener("click", playRound));
+
+
 function getComputerChoice () {
     let random = Math.floor(Math.random()*3);
     switch (random) {
@@ -10,75 +16,77 @@ function getComputerChoice () {
     }
 }
 
-function playRound (playerChoice, computerChoice) {
-    let result;
+function playRound (event) {
+    let playerChoice = event.target.textContent;
+    console.log(playerChoice);
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    const result = document.createElement("p");
+    
     if (playerChoice === computerChoice) {
-        result = "draw";
-        console.log(`It's a draw! You both played ${playerChoice}.`);
-        return result;
+        result.textContent = `It's a draw! You both played ${playerChoice}.`;
+        output.appendChild(result);
+        return;
     }
     switch (playerChoice) {
         case "Rock":
             if (computerChoice === "Paper") {
-                console.log("You lose! Paper covers Rock.");
-                result = "loss";
+                result.textContent = "You lose! Paper covers Rock.";
+                break;
             } else {
-                console.log("You win! Rock breaks Scissors.");
-                result ="win";
+                result.textContent = "You win! Rock breaks Scissors.";
+                break;
             }            
-            return result;
         case "Paper":
             if (computerChoice === "Scissors") {
-                console.log("You lose! Scissors cut Paper.");
-                result = "loss";
+                result.textContent = "You lose! Scissors cut Paper.";
+                break;
             } else {
-                console.log("You win! Paper covers Rock.");
-                result ="win";         
+                result.textContent = "You win! Paper covers Rock.";
+                break;       
             }
-            return result;
         default:
             if (computerChoice === "Rock") {
-                console.log("You lose! Rock breaks Scissors.");
-                result = "loss";
+                result.textContent = "You lose! Rock breaks Scissors.";
+                break;
             } else {
-                console.log("You win! Scissors cut Paper.");
-                result ="win";         
+                result.textContent = "You win! Scissors cut Paper.";
+                break;        
             }
-            return result;
     }
-}
-
-function playGame () {
-    let tally = 0;
-    for (let i = 1; i <=5 ; i++) {
-        let playerChoice = prompt(`Round ${i} : Rock, Paper or Scissors?`);
-        if (playerChoice.length < 4) {
-            playerChoice = prompt("Too short. Rock, Paper or Scissors?");
-        }
-        console.log(playerChoice);
-        playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
-        if (!(playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors")) {
-            console.log ("Not a valid move.");
-            i = i-1;
-            continue;
-        }
-        console.log(`Round ${i}`);
-
-        let result = playRound(playerChoice, getComputerChoice());
-        switch (result) {
-            case "win":
-                tally = tally + 1;
-                break;
-            case "loss":
-                tally = tally - 1;
-                break;
-            default:
-                break;
-        }
-    }
-    let finalResult = (tally >=1) ? "Yay, you won!" : (tally < 0) ? "Oh no, you lost :(" : "It's a draw.";
-    console.log (finalResult); 
+    output.appendChild(result);
+    return;
 }
 
 
-playGame();
+// function playGame () {
+//     let tally = 0;
+//     for (let i = 1; i <=5 ; i++) {
+//         let playerChoice = prompt(`Round ${i} : Rock, Paper or Scissors?`);
+//         if (playerChoice.length < 4) {
+//             playerChoice = prompt("Too short. Rock, Paper or Scissors?");
+//         }
+//         console.log(playerChoice);
+//         playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
+//         if (!(playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors")) {
+//             console.log ("Not a valid move.");
+//             i = i-1;
+//             continue;
+//         }
+//         console.log(`Round ${i}`);
+
+//         let result = playRound(playerChoice, getComputerChoice());
+//         switch (result) {
+//             case "win":
+//                 tally = tally + 1;
+//                 break;
+//             case "loss":
+//                 tally = tally - 1;
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+//     let finalResult = (tally >=1) ? "Yay, you won!" : (tally < 0) ? "Oh no, you lost :(" : "It's a draw.";
+//     console.log (finalResult); 
+// }
